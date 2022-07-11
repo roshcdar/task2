e10bee9f5d5d9522ea4f4ffa9b1afc37ac1cd273
roshcdar <roshcdar@cvut.cz>
2022-07-12 00:03:30 +0300

    Second version improved.

--- src/synch.cpp
+++ src/synch.cpp
@@ -8,6 +8,22 @@
 
 namespace fs = std::filesystem;
 
+/**
+ * Read source folder path, replica folder path, synchronization interval, log file path from input.
+ *
+ * Source folder path, replica folder path, synchronization interval and log file path are read from standard input.
+ * Reading is not successful:
+ * - if source folder path is not a valid string or is not a folder path,
+ * - if replica folder path is not a valid string,
+ * - if synchronization interval is not a valid integer or is less than 0,
+ * - if log file path is not a valid string
+ *
+ * @param[out] source   source folder path
+ * @param[out] replica  replica folder path
+ * @param[out] interval synchronization interval
+ * @param[out] log      log file path
+ * @returns true if reading is successful, false otherwise
+ */
 bool readInput(std::string & source, std::string & replica, int & interval, std::string & log) {
     std::cout << "Enter source folder path:" << std::endl;
     if (!(std::cin >> source) || !fs::is_directory(source))
@@ -16,7 +32,7 @@ bool readInput(std::string & source, std::string & replica, int & interval, std:
     if (!(std::cin >> replica))
         return false;
     std::cout << "Enter synchronization interval in seconds:" << std::endl;
-    if (!(std::cin >> interval || interval < 0))
+    if (!(std::cin >> interval) || interval < 0)
         return false;
     std::cout << "Enter log file path:" << std::endl;
     if (!(std::cin >> log))
@@ -24,15 +40,32 @@ bool readInput(std::string & source, std::string & replica, int & interval, std:
     return true;
 }
 
+/**
+ * Remove all content of a folder.
+ *
+ * @param[in] folder folder path
+ */
 void removeFolderContent(const std::string& folder)
 {
     for (const auto& entry : fs::directory_iterator(folder))
         fs::remove_all(entry.path());
 }
 
-enum OperationType{created, removed, copied};
+/**
+ * Enum class representing operations, which are performed on a file.
+ */
+enum operationType{created, removed, copied};
 
-void logOperation(const std::string & path,  std::ostream & os, OperationType operationType) {
+/**
+ * Log a operation, which is performed on a file.
+ *
+ * Operation, which is performed on a file, is logged to output stream.
+ *
+ * @param[in] path          file path
+ * @param[out] os           output stream
+ * @param[in] operationType operation, which is performed on a file
+ */
+void logOperation(const std::string & path,  std::ostream & os, operationType operationType) {
     switch(operationType) {
         case created: {
             os << path + " is created." << std::endl;
@@ -49,17 +82,35 @@ void logOperation(const std::string & path,  std::ostream & os, OperationType op
     }
 }
 
-void synchronize(const std::string & source, const std::string & replica, int interval, const std::string & log, bool & quiet) {
-    std::set<std::string> lastLog, currentLog;
-    std::set<std::string>::iterator it;
-    std::string path;
+/**
+ * Synchronize two folders: source and replica and log operations performed on files in source folder.
+ *
+ * Source and replica folders are periodically synchronized. The synchronization is one-way.
+ * After the synchronization content of the replica folder is  exactly matched content of the source folder.
+ * The information about what files were copied from source folder to replica folder during synchronization is logged to
+ * a log file and standard output. The information about what files were created and removed during synchronization interval
+ * is logged to a log file and standard output too.
+ * Synchronization is quited, when signal to quit is true.
+ *
+ * @param[in] source    source file path
+ * @param[in] replica   replica file path
+ * @param[in] interval  synchronization interval
+ * @param[in] log       log file path
+ * @param[in] quit      signal to quit
+ * @returns true if synchronization is successfully quited, false otherwise
+ */
+bool synchronize(const std::string & source, const std::string & replica, int interval, const std::string & log, const bool & quit) {
     std::ofstream logFile;
     logFile.open(log);
-    if (!logFile.is_open()) {
-        std::cout << "Error opening file. Enter quiet to end." << std::endl;
-        return;
-    }
-    while(!quiet) {
+    if (!logFile.is_open())
+        return false;
+    /// Paths to files, which were copied during last synchronization.
+    std::set<std::string> lastLog;
+    /// Paths to files, which are copied during current synchronization.
+    std::set<std::string> currentLog;
+    std::set<std::string>::iterator it;
+    std::string path;
+    while(!quit) {
         std::this_thread::sleep_for(std::chrono::seconds(interval));
         removeFolderContent(replica);
         fs::copy(source, replica, fs::copy_options::overwrite_existing | fs::copy_options::directories_only | fs::copy_options::recursive);
@@ -85,19 +136,27 @@ void synchronize(const std::string & source, const std::string & replica, int in
             logOperation(str, logFile, removed);
             logOperation(str, std::cout, removed);
         }
-        logFile << std::endl;
-        std::cout << std::endl;
-        lastLog = currentLog;
-        currentLog.clear();
+        if (!lastLog.empty() || !currentLog.empty()) {
+            logFile << std::endl;
+            std::cout << std::endl;
+            lastLog = currentLog;
+            currentLog.clear();
+        }
     }
     logFile.close();
+    return true;
 }
 
-void waitForEnd(bool & quiet) {
+/**
+ * Waits until user enters quit. After quit is entered, signal to quit is set on true.
+ *
+ * @param[out] quit   signal to quit
+ */
+void waitForQuit(bool & quit) {
     std::string line;
     while (getline(std::cin, line)) {
-        if (line == "quiet") {
-            quiet = true;
+        if (line == "quit") {
+            quit = true;
             break;
         }
     }
@@ -112,8 +171,9 @@ int main() {
     }
     if (!fs::is_directory(replica))
         fs::create_directories(replica);
-    bool quiet = false;
-    std::thread t1(waitForEnd, std::ref(quiet));
-    synchronize(source, replica, interval, log, quiet);
+    bool quit = false;
+    std::thread t1(waitForQuit, std::ref(quit));
+    if (!synchronize(source, replica, interval, log, quit))
+        std::cout << "Error opening file. Enter quit to end." << std::endl;
     t1.join();
 }
